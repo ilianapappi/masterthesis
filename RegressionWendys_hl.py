65a1a41a416952b817ea12a82b2f40b05a67d435
ilianapappi <iliana.n.pappi@gmail.com>
2017-07-05 11:34:44 +0200

    Add files via upload

---
+++ RegressionWendys_hl.py
@@ -0,0 +1,113 @@
+import numpy as np
+import pandas as pd
+from sklearn.svm import SVR
+from sklearn.preprocessing import normalize
+from sklearn import model_selection
+from sklearn.decomposition import PCA
+import scipy
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+#from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neural_network import MLPRegressor
+import math
+from sklearn.model_selection import train_test_split
+
+#------------------------------------------------------------>
+
+df1 = pd.read_csv('/home/11394153/featureskeras/wendys_hl_clean_nindexed.csv',header=None)
+print df1.shape
+
+X_train = df1.loc[0:5965,:].values
+print X_train.shape
+print(type(X_train))
+
+X_test = df1.loc[5966:,:].values
+print X_test.shape
+print(type(X_test))
+
+df2 = pd.read_excel('/home/11394153/data/datatxt/wendys2_clean.xlsx')
+df3 = df2['imageLikeCount']
+print df3.shape
+
+Y_train = df3.loc[0:5965].values
+Y_train = Y_train.reshape((Y_train.shape[0],1))
+print(type(Y_train))
+print Y_train.shape
+
+Y_test = df3.loc[5966:].values
+Y_test = Y_test.reshape((Y_test.shape[0],1))
+print(type(Y_test))
+print Y_test.shape
+
+#------------------------------------------------------------>
+
+X_train = normalize(X_train,norm='l1')
+print X_train.shape
+X_test = normalize(X_test,norm='l1')
+print X_test.shape
+
+Y_train = np.log(Y_train+np.ones((Y_train.shape[0],1)))
+print Y_train.shape
+Y_test = np.log(Y_test+np.ones((Y_test.shape[0],1)))
+print Y_test.shape
+np.save('YtestWENDYS.npy',Y_test)
+
+#--------------------------------------------------------------->
+
+#lsC = [0.01,0.1,1,10,100,1000]
+
+#for C in lsC:
+print "SVR"
+#print C
+modelHLvisual= SVR(C=100)
+modelHLvisual.fit(X_train,Y_train)
+predictionsHL = modelHLvisual.predict(X_test)
+np.save('YpredWENDYShl.npy',predictionsHL)
+print predictionsHL.shape
+        
+spearmanscoeff = scipy.stats.spearmanr(Y_test,predictionsHL)
+print spearmanscoeff
+
+
+print "RandomForrest100"
+
+model4 = RandomForestRegressor(n_estimators=100)
+
+model4.fit(X_train,Y_train)
+
+preds4 = model4.predict(X_test)
+print preds4.shape
+
+spearmanscoeff4 = scipy.stats.spearmanr(Y_test,preds4)
+print spearmanscoeff4
+
+print "MLP"
+
+model5 = MLPRegressor()
+model5.fit(X_train,Y_train)
+
+preds5 = model5.predict(X_test)
+print preds5.shape
+
+spearmanscoeff5 = scipy.stats.spearmanr(Y_test,preds5)
+print spearmanscoeff5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ avgpool.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+import scipy.stats
+
+
+
+print 'pool action'
+
+predictionsHLa = np.load('/home/11394153/Regression/YpredACTIONhl_MLP.npy')
+
+predictionsLLa = np.load('/home/11394153/Regression/YpredACTIONll_MLP.npy')
+
+predictionsVSa = np.load('/home/11394153/Regression_vsent/YpredACTIONvs_MLP.npy')
+
+act = np.vstack((predictionsHLa,predictionsLLa,predictionsVSa))
+
+print act.shape
+
+meanact = np.mean(act, axis=0)
+
+print meanact.shape
+
+Ytestact = np.load('/home/11394153/Regression/YtestACTION.npy')
+
+print Ytestact.shape
+
+
+spearmanscoeff1 = scipy.stats.spearmanr(Ytestact,meanact)
+print spearmanscoeff1
+
+print 'pool scene'
+
+predictionsHLs = np.load('/home/11394153/Regression/YpredSCENEhl_MLP.npy')
+
+predictionsLLs = np.load('/home/11394153/Regression/YpredSCENESll_MLP.npy')
+
+predictionsVSs = np.load('/home/11394153/Regression_vsent/YpredSCENEvs_MLP.npy')
+
+sc = np.vstack((predictionsHLs,predictionsLLs,predictionsVSs))
+
+print sc.shape
+
+meansc = np.mean(sc, axis=0)
+
+print meansc.shape
+
+Ytestsc = np.load('/home/11394153/Regression/YtestSCENE.npy')
+
+print Ytestsc.shape
+
+
+
+spearmanscoeff2 = scipy.stats.spearmanr(Ytestsc,meansc)
+print spearmanscoeff2
+
+print "pp"
+
+predictionsHLpp = np.load('/home/11394153/Regression/YpredPEOPETShl_MLP.npy')
+
+predictionsLLpp = np.load('/home/11394153/Regression/YpredPEOPETSll_MLP.npy')
+
+predictionsVSpp = np.load('/home/11394153/Regression_vsent/YpredPEOPETSvs_MLP.npy')
+
+pp = np.vstack((predictionsHLpp,predictionsLLpp,predictionsVSpp))
+
+print pp.shape
+
+meanpp = np.mean(pp, axis=0)
+
+print meanpp.shape
+
+Ytestpp = np.load('/home/11394153/Regression/YtestPEOPETS.npy')
+
+print Ytestpp.shape
+
+
+spearmanscoeff3 = scipy.stats.spearmanr(Ytestpp,meanpp)
+print spearmanscoeff3
+
+print 'wendys'
+
+
+predictionsHLw = np.load('/home/11394153/Regression/YpredWENDYShl_MLP.npy')
+
+predictionsLLw = np.load('/home/11394153/Regression/YpredWENDYSll_MLP.npy')
+
+predictionsVSw = np.load('/home/11394153/Regression_vsent/YpredWENDYSvs_MLP.npy')
+
+print predictionsHLw.shape, predictionsLLw.shape, predictionsVSw.shape
+
+w = np.vstack((predictionsHLw,predictionsLLw,predictionsVSw))
+
+print w.shape
+
+meanw = np.mean(w, axis=0)
+
+print meanw.shape
+
+Ytestw = np.load('/home/11394153/Regression/YtestWENDYS.npy')
+
+print Ytestw.shape
+
+
+spearmanscoeff4 = scipy.stats.spearmanr(Ytestw,meanw)
+print spearmanscoeff4
+
+
+
+
+
+
+
